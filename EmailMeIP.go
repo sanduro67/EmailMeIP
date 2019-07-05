65411b61174fd9795b0be255612e537be1aa3b0d
sanduro67 <32141187+sanduro67@users.noreply.github.com>
2019-07-05 07:21:26 +0200

    Add files via upload

---
+++ EmailMeIP.go
@@ -0,0 +1,19 @@
+package main
+import "os"
+import "fmt"
+func main() {
+IP := a *IPAddr
+fmt.Printf("%v\n",len(os.Args))
+fmt.Printf(IP)
+e: EmailMeIP <email_adress>")
+  return
+}
+    email := os.Args[1]
+
+    // arg := os.Args[3]
+
+    // fmt.Println(argsWithProg)
+    fmt.Println(email)
+    // fmt.Println(arg)
+
+}
